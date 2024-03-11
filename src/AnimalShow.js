39bacbac0c7c4f7797850bc26713bc9ca31fec67
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-11 12:05:50 +0800

    Add mapping for type to animal image

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,5 +1,35 @@
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
+
+// The actual mapping from type to image
+// const svgMap = {
+//     bird: bird,
+//     cat: cat,
+//     cow: cow,
+//     dog: dog,
+//     gator: gator,
+//     horse: horse
+// };
+
+// Use shortcut since type that is passed is same with image name
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    horse
+};
+
 function AnimalShow({ type }) {
-    return <div>{type}</div>
+    return <div>
+        <img alt='animal' src={svgMap[type]} />
+    </div>
 }
 
 export default AnimalShow;
