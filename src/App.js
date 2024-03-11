b80b969d970802d46f28b411d9e8f7d9c63ecd09
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-11 10:33:52 +0800

    Add state count

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
+
 function App() {
 
+  const [count, setCount] = useState(0);
+
   const handleClick = () => {
-    console.log('Button was clicked!');
+    setCount(count + 1);
   }
 
   return (
     <div>
+
       <button onClick={handleClick}>Add Animal</button>
+
+      <div>
+        Number of animals: {count}
+      </div>
+
     </div>
   );
 
