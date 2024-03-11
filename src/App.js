f3e787eadad4da17dda1af427c47bb7c03f9efd7
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-11 10:30:18 +0800

    Add event onClick

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 function App() {
+
+  const handleClick = () => {
+    console.log('Button was clicked!');
+  }
+
   return (
     <div>
-      Show animal list here!
+      <button onClick={handleClick}>Add Animal</button>
     </div>
   );
+
 }
 
 export default App;
